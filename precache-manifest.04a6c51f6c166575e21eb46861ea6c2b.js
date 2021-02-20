1bf5927a2f2328d70b6465a2b72bd2727220094c
Shaunmak1214 <shaonmak@gmail.com>
2021-02-21 05:41:29 +0800

    Updates

---
+++ precache-manifest.04a6c51f6c166575e21eb46861ea6c2b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "674762e1977b8021d5cc0f025b28ff56",
+    "url": "/React-Weather-App/index.html"
+  },
+  {
+    "revision": "362ef9d744a3a7521ea4",
+    "url": "/React-Weather-App/static/css/main.6a7e7d7b.chunk.css"
+  },
+  {
+    "revision": "5fae6b1de25c9e99106f",
+    "url": "/React-Weather-App/static/js/2.0258b798.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Weather-App/static/js/2.0258b798.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "362ef9d744a3a7521ea4",
+    "url": "/React-Weather-App/static/js/main.b0c425c3.chunk.js"
+  },
+  {
+    "revision": "43f201cf362bbfa1c93d",
+    "url": "/React-Weather-App/static/js/runtime-main.4ed83f9c.js"
+  },
+  {
+    "revision": "63c1ea58cee7eb6f6bf9fe756f7abb8c",
+    "url": "/React-Weather-App/static/media/autumn.63c1ea58.jpg"
+  },
+  {
+    "revision": "00d428ae46347e159408896b8a429056",
+    "url": "/React-Weather-App/static/media/search.00d428ae.svg"
+  },
+  {
+    "revision": "ede9763d00b629e9a8472c46d75b0850",
+    "url": "/React-Weather-App/static/media/winter.ede9763d.jpg"
+  }
+]);
